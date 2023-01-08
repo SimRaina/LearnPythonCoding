53e2dbd7275eeb0239477b9768b37e4cf0d604c2
BusyQA <github.busyqa@gmail.com>
2023-01-08 15:18:34 +0530

    Added Class Variables

--- 12_Classes_Instances.py
+++ 12_Classes_Instances.py
@@ -1,4 +1,5 @@
 class Employee:
+    dept = 'IT' # class variable
     def __init__(self, first, last, role):
         self.first = first
         self.last = last
@@ -8,9 +9,21 @@ class Employee:
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
 
+    def deptname(self):
+         return self.dept
+         # return Employee.dept
+
 emp1 = Employee('Sim', 'Raina', 'QA')
 
 print(emp1.email)
 print(emp1.fullname())
 
 print(Employee.fullname(emp1))
+# print(emp1.deptname())
+# emp1.dept = 'HR'
+Employee.dept = 'HR'
+print(emp1.dept)
+print(Employee.dept)
+
+# print(emp1.__dict__)
+# print(Employee.__dict__)
