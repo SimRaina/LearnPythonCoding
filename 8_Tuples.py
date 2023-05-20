844eb56a0f2a3efef505eae8833f2c078917152b
Sim Raina QA <simran.pre90@gmail.com>
2023-05-20 21:47:30 +0530

    Updated Tuple

--- 8_Tuples.py
+++ 8_Tuples.py
@@ -1,10 +1,44 @@
-tuple1 = ('Physics', 'Chemistry', 'Math', 'Computers')
-tuple2 = tuple1
+# Creating a tuple
+person = ("John", 25, "USA")
 
-print(tuple1)
-print(tuple2)
+# Tuples are immutable
+person[0] = "Test"
+print(person)
 
-tuple1[0] = 'Test' # error immutable
+# Accessing tuple elements
+#print(person[0])  # Output: John
+#print(person[1])  # Output: 25
+#print(person[2])  # Output: USA
 
-print(tuple1)
-print(tuple2)
+# Tuples Unpacking
+name, age, country = person
+print(name)      # Output: John
+print(age)       # Output: 25
+print(country)   # Output: USA
+
+
+# Tuple with different data types
+mixed_tuple = ("apple", 10, True, 3.14)
+
+# Iterating over a tuple
+#for item in mixed_tuple:
+    #print(item)
+
+# Tuple concatenation
+tuple1 = (1, 2, 3)
+tuple2 = (4, 5, 6)
+concatenated_tuple = tuple1 + tuple2
+#print(concatenated_tuple)  
+
+numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9)
+# Tuple length
+#print(len(numbers))  # Output: 9
+
+# Nested tuples
+nested_tuple = ((1, 2), (3, 4), (5, 6))
+#print(nested_tuple[0][0])  # Output: 3
+
+
+
+
+
